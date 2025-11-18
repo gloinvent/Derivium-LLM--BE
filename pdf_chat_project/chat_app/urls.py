26c01e545a3740c15ce4c5ddd505d4ef0c7fe627
sonali <sonali@gloify.com>
2025-11-18 11:35:17 +0530

    realtime pdf status checking added

--- pdf_chat_project/chat_app/urls.py
+++ pdf_chat_project/chat_app/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('check_processing_status/<int:pdf_id>/', views.check_processing_status, name='check_processing_status'),
     path('chat_history/<int:pdf_id>/', views.get_chat_history, name='get_chat_history'),
     path('clear_data/', views.clear_data, name='clear_data'),
+    # Debug endpoints
+    path('pdf-status/<int:pdf_id>/', views.pdf_status_simple, name='pdf_status_simple'),
+    path('all-pdfs-status/', views.get_all_pdfs_status, name='get_all_pdfs_status'),
+    path('debug-pdf/<int:pdf_id>/', views.debug_pdf_status, name='debug_pdf_status'),
+    path('list-pdfs/', views.list_pdfs, name='list_pdfs'),
 ]
